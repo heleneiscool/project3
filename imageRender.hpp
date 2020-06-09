ca2b96a4f45ec80716f309d6b4f8eb4289a8d9d4
NadyaVUW <66417647+NadyaVUW@users.noreply.github.com>
2020-06-09 20:49:12 +1200

    Update imageRender.hpp

--- imageRender.hpp
+++ imageRender.hpp
@@ -1,27 +1,41 @@
 #include "robot.hpp"
-int** renderImageCore(){ //2 asterix to return 2d array
+
+int* renderImageCore(){
+
 	takePicture();
+
     SavePPMFile("i0.ppm",cameraView);
-    int imageSize = cameraView.height * cameraView.width; //array that holds arrays
-	int  pixels[cameraView.height][cameraView.width]; //note: i reliase that there is a better way to set a list length to the value of a varible but i cant remeber it, so please do update this
+
+    int imageSize = cameraView.height * cameraView.width;
+
+	int*  pixels[imageSize]; 
+//scan horizontal array and then divide all by 2 and if pix > 200 then its white and then change motor speed. if n of array is >200 on right side then lower speed vRight
+
 	
 	OpenPPMFile("i0.ppm", cameraView);
-    // for pixel in image 
+    // for pixel in image
+
 		//get the pixel:
-	Pixel curPix; // current pixel
-	for(int col = 0; col < cameraView.width; col ++){
-	for ( int row = 0; row < camera.View.height; row++){
-		curPix = get_pixel(row, col, 3); //will get the 3 values of rgb
-		if(curPix > 200){ //if its basically white
-		pixels[row][col]= 1; // will go through entire image one pixel at a time, put value in curpix
-		}else {
-			pixels[row][col]= 0;
+		int curPix; //current pixel get_pixel( ImagePPM image,int row,int col, int color)
+		for(int col = 0; col < cameraView.width; col++){
+			for(int row = 0; row < cameraView.height; row++){
+				curPix = get_pixel(cameraView, row, col, 3);
+				if(curPix > 200) {
+					pixels[row] = 1;
+				}else {
+					pixels[row] = 0;
+				}	
+				
+			}
 		}
-		
-	}
-	}
+		std::cout<<pixels<<endl;
+
 		//if white put 1 in the array at the index of that pixel
+
 		//otherwise put 0 in that index
+
 	//should return the list of 1s and 0s
+
 	return pixels;
+
 }
