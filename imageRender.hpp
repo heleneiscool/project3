c223b91c57742a9c7da0be5b8a7f20f37fb2e961
heleneiscool <46540694+heleneiscool@users.noreply.github.com>
2020-06-19 16:10:13 +1200

    Update imageRender.hpp

--- imageRender.hpp
+++ imageRender.hpp
@@ -1,41 +1,115 @@
 #include "robot.hpp"
+/*****Line reading, values for lines from image*****/
+struct LineReading{
+	double leftLine=0;
+	double leftPosition=0;
+	double rightLine=0;
+	double rightPosition=0;
+	double topLine=0;
+	double topPosition=0;
+	double bottomLine=0;
+	double bottomPosition=0;
+};
 
-int* renderImageCore(){
+struct RedLines{
+	int redRow=0;
+	int leftSide=0;
+	int rightSide=0;
+};
 
-	takePicture();
-
-    SavePPMFile("i0.ppm",cameraView);
-
-    int imageSize = cameraView.height * cameraView.width;
-
-	int*  pixels = new int[imageSize]; 
-//scan horizontal array and then divide all by 2 and if pix > 200 then its white and then change motor speed. if n of array is >200 on right side then lower speed vRight
-
-	
-	OpenPPMFile("i0.ppm", cameraView);
-    // for pixel in image
-
-		//get the pixel:
-		int curPix; //current pixel get_pixel( ImagePPM image,int row,int col, int color)
-		for(int col = 0; col < cameraView.width; col++){
-			for(int row = 0; row < cameraView.height; row++){
-				curPix = get_pixel(cameraView, row, col, 3);
-				if(curPix > 200) {
-					pixels[row*col+col] = 1;
-				}else {
-					pixels[row*col+col] = 0;
-				}	
-				
+LineReading renderImageCompletion(){
+	LineReading reading;
+			for(int row=0; row<cameraView.height; row++){
+				for(int col=0; col<cameraView.width; col++){
+					int brightness = get_pixel(cameraView, row, col, 3);
+					bool isWhite = brightness>=250; //includes black so that the car hits the flag in the end
+					if(isWhite){
+					if(col == 0){                                
+						reading.leftPosition += row;
+						reading.leftLine++;
+					}
+					else if(col == cameraView.width-1){
+						
+				 reading.rightPosition += row;
+				 reading.rightLine++;
+					}
+					if(row == 0){
+						
+					reading.topPosition += col;
+				 reading.topLine++;
+						}
+						else if(row==cameraView.height-1){
+							
+							reading.bottomPosition += col;
+							reading.bottomLine++;
+						}
+				}
+				}
 			}
-		}
-		std::cout<<pixels<<endl;
+	return reading;
+}
 
-		//if white put 1 in the array at the index of that pixel
+RedLines renderImageChallenge(){
+	
+	int checkRow = 80;
+	int redRow = 0;
+	for(int col=0; col<cameraView.width; col++){
+		int redness = get_pixel(cameraView, redRow, col, 0);
+			int nonRed = (get_pixel(cameraView, redRow, col, 1)+get_pixel(cameraView, redRow, col, 2))/2;
+			if(redness>nonRed*1.05){
+				redRow++;
+			}
+			else{
+				break;
+			}
+	}
 
-		//otherwise put 0 in that index
+	int leftSide=0;
+	bool checking = false;
+	for(int col=0; col<cameraView.width; col++){
+	for(int row=0; row<cameraView.height; row++){
+		
+		int redness = get_pixel(cameraView, row, col, 0);
+			int nonRed = (get_pixel(cameraView, row, col, 1)+get_pixel(cameraView, row, col, 2))/2;
+			if(redness>nonRed*1.05 & !checking){
+				checking = true;
+			}
+			if(redness>nonRed*1.05 & checking){
+				leftSide++;
+			}
+			else{
+				checking = false;
+				break;
+			}
+	}
+}
+int rightSide=0;
 
-	//should return the list of 1s and 0s
+checking = false;
+	for(int col=cameraView.width-1; col>0; col--){
+	for(int row=0; row<cameraView.height; row++){
+		
+		int redness = get_pixel(cameraView, row, col, 0);
+			int nonRed = (get_pixel(cameraView, row, col, 1)+get_pixel(cameraView, row, col, 2))/2;
+			if(redness>nonRed*1.05 & !checking){
+				checking =true;
+			}
+			if(redness>nonRed*1.05 & checking){
+				rightSide++;
+			}
+			else{
+				checking=false;
+				break;
+			}
+	}
+}
 
-	return pixels;
+	std::cout<<"left"<<leftSide<<" right="<<rightSide<<" redrow="<<redRow<<std::endl;
+	int checkRight = cameraView.width-1;
+	RedLines lines;
+	lines.redRow = redRow;
+	lines.rightSide = rightSide;
+	lines.leftSide = leftSide;
+	return lines;
 
 }
