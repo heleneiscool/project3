867cbf4e5c6a86ad33d2d1c42886c02e15c7e257
heleneiscool <46540694+heleneiscool@users.noreply.github.com>
2020-06-19 16:28:09 +1200

    Update imageRender.hpp

--- imageRender.hpp
+++ imageRender.hpp
@@ -1,72 +1,121 @@
-void setMotorCompletion(LineReading reading){
-		
-		double vLeft = 40.0;
-		double vRight = 40.0;
-		const double v_go = 40.0;
-		const double kp = 1; //how far the error is from the error line, the constant turning SCALE 
-        double dv = 0;
-        double delta = 20;
-        if(reading.rightLine>0){
-			//go right if there is a line to the right
-			 double row = reading.rightPosition/reading.rightLine;
-			 delta = (row/cameraView.height)*(15);
-			vLeft = vLeft + 5+delta;
-			 vRight = vRight- 5-delta;
+#include "robot.hpp"
+/*****Line reading, values for lines from image*****/
+struct LineReading{
+	double leftLine=0;
+	double leftPosition=0;
+	double rightLine=0;
+	double rightPosition=0;
+	double topLine=0;
+	double topPosition=0;
+	double bottomLine=0;
+	double bottomPosition=0;
+};
+
+struct RedLines{
+	int redRow=0;
+	int leftSide=0;
+	int rightSide=0;
+};
+
+LineReading renderImageCompletion(){
+	LineReading reading;
+	for(int row=0; row<cameraView.height; row++){
+		for(int col=0; col<cameraView.width; col++){
+			int brightness = get_pixel(cameraView, row, col, 3);
+			bool isWhite = brightness>=250; //includes black so that the car hits the flag in the end
+			if(isWhite){
+				if(col == 0){                                
+					reading.leftPosition += row;
+					reading.leftLine++;
+				}
+				else if(col == cameraView.width-1){
+							
+					reading.rightPosition += row;
+					reading.rightLine++;
+				}
+				if(row == 0){
+					reading.topPosition += col;
+					reading.topLine++;
+				}
+				else if(row==cameraView.height-1){
+					reading.bottomPosition += col;
+					reading.bottomLine++;
+				}
 			}
-		else if(reading.topLine==0 && reading.bottomLine>0 && reading.leftLine==0){
-			vLeft=-40;
-			vRight =0;
-		
 		}
-         else if(reading.topLine>0){
-			 //go foward if no line to the right but there is one foward
-			 double col = reading.topPosition/reading.topLine;
-			 double difference = cameraView.width/2 - col;
-			 double delta = 30*(difference/cameraView.width);
-			 vLeft -= delta;
-			 vRight += delta;
-		 }
-		 else if(reading.leftLine>0){
-			 //go left if there is no line ahead or to right
-			 double row = reading.leftPosition/reading.leftLine;
-			 delta = (row/cameraView.height)*15;
-			vLeft = vLeft - 5-delta;
-			 vRight = vRight+ 5+delta;
+	}
+	return reading;
+}
+
+RedLines renderImageChallenge(){
+	int checkRow = 70; //the row which is checked
+	int redRow = 0; //varible stores the size of red line
+	/* for each col, check if the 'checkrow' row is red. if so, start counting/increase the number of red pixels in that row */
+	for(int col=0; col<cameraView.width; col++){
+		int redness = get_pixel(cameraView, redRow, col, 0);
+			int nonRed = (get_pixel(cameraView, redRow, col, 1)+get_pixel(cameraView, redRow, col, 2))/2;
+			if(redness>nonRed*1.05){
+				redRow++;
 			}
-		else{
-			vLeft =-40;
-			vRight =-40;
-		}		
-
-    std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-
-	setMotors(vLeft,vRight);   
-     //usleep(10000);
-     std::cout<<std::endl<<"left:"<<reading.leftPosition/(1+reading.leftLine)<<" right:"<<reading.rightPosition/(1+reading.rightLine)<<" top:"<<reading.topPosition/(1+reading.topLine)<<" bottom:"<<reading.bottomPosition/(1+reading.bottomLine)<<std::endl;    // Prints the average
-
-}
-
-void setMotorChallenge(RedLines border){
-	LineReading checkIfWhite = renderImageCompletion();
-	if(checkIfWhite.topLine>0){
-		//go foward if no line to the right but there is one foward
-		double col = checkIfWhite.topPosition/checkIfWhite.topLine;
-		double difference = cameraView.width/2 - col;
-		double delta = 30*(difference/cameraView.width);
-		double vLeft =40- delta;
-		double vRight =40+ delta;
-		setMotors(vLeft,vRight);  
+			else{
+				break;
+			}
 	}
-	else{
-		if(border.redRow>cameraView.width*0.8){
-		setMotors(50, 10); 
+
+	int leftSide=0; //size of left red border
+	bool checking = false; //when true, the border has been found
+	/* for columns from the left, counting the size of left side */
+	for(int col=0; col<cameraView.width; col++){
+		int count=0;
+	for(int row=0; row<cameraView.height; row++){
+		
+		int redness = get_pixel(cameraView, row, col, 0);
+			int nonRed = (get_pixel(cameraView, row, col, 1)+get_pixel(cameraView, row, col, 2))/2;
+			if(redness>nonRed*1.05 & !checking){
+				checking = true;
+			}
+			if(redness>nonRed*1.05 & checking){
+				count++;
+			}
+			else{
+				checking = false;
+				break;
+			}
 	}
-	else if(border.rightSide==0){
-		setMotors(60,20);
-	}
-	else{
-		int difference = border.leftSide-border.rightSide;
-		setMotors(40, 40);
+	if(count>0){
+		leftSide++;
 	}
 }
+int rightSide=0;
+/* counts the number of columns with red from the right (in the red-most section of red) */
+checking = false;
+	for(int col=cameraView.width-1; col>0; col--){
+		int count=0;
+	for(int row=0; row<cameraView.height; row++){
+		int redness = get_pixel(cameraView, row, col, 0);
+			int nonRed = (get_pixel(cameraView, row, col, 1)+get_pixel(cameraView, row, col, 2))/2;
+			if(redness>nonRed*1.05 & !checking){
+				checking =true;
+			}
+			if(redness>nonRed*1.05 & checking){
+				count++;
+			}
+			else{
+				checking=false;
+				break;
+			}
+	}
+	if(count>0){
+	rightSide++;
+}
+}
+
+	std::cout<<"left"<<leftSide<<" right="<<rightSide<<" redrow="<<redRow<<std::endl;
+	int checkRight = cameraView.width-1;
+	RedLines lines;
+	lines.redRow = redRow;
+	lines.rightSide = rightSide;
+	lines.leftSide = leftSide;
+	return lines;
+
 }
