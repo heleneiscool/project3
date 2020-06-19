982e50de6efa17c96899af7a85bf4fe21c6a499d
heleneiscool <46540694+heleneiscool@users.noreply.github.com>
2020-06-19 16:21:48 +1200

    Update imageRender.hpp

--- imageRender.hpp
+++ imageRender.hpp
@@ -19,40 +19,38 @@ struct RedLines{
 
 LineReading renderImageCompletion(){
 	LineReading reading;
-			for(int row=0; row<cameraView.height; row++){
-				for(int col=0; col<cameraView.width; col++){
-					int brightness = get_pixel(cameraView, row, col, 3);
-					bool isWhite = brightness>=250; //includes black so that the car hits the flag in the end
-					if(isWhite){
-					if(col == 0){                                
-						reading.leftPosition += row;
-						reading.leftLine++;
-					}
-					else if(col == cameraView.width-1){
-						
-				 reading.rightPosition += row;
-				 reading.rightLine++;
-					}
-					if(row == 0){
-						
-					reading.topPosition += col;
-				 reading.topLine++;
-						}
-						else if(row==cameraView.height-1){
-							
-							reading.bottomPosition += col;
-							reading.bottomLine++;
-						}
+	for(int row=0; row<cameraView.height; row++){
+		for(int col=0; col<cameraView.width; col++){
+			int brightness = get_pixel(cameraView, row, col, 3);
+			bool isWhite = brightness>=250; //includes black so that the car hits the flag in the end
+			if(isWhite){
+				if(col == 0){                                
+					reading.leftPosition += row;
+					reading.leftLine++;
 				}
+				else if(col == cameraView.width-1){
+							
+					reading.rightPosition += row;
+					reading.rightLine++;
+				}
+				if(row == 0){
+					reading.topPosition += col;
+					reading.topLine++;
+				}
+				else if(row==cameraView.height-1){
+					reading.bottomPosition += col;
+					reading.bottomLine++;
 				}
 			}
+		}
+	}
 	return reading;
 }
 
 RedLines renderImageChallenge(){
-	
-	int checkRow = 80;
-	int redRow = 0;
+	int checkRow = 70; //the row which is checked
+	int redRow = 0; //varible stores the size of red line
+	/* for each col, check if the 'checkrow' row is red. if so, start counting/increase the number of red pixels in that row */
 	for(int col=0; col<cameraView.width; col++){
 		int redness = get_pixel(cameraView, redRow, col, 0);
 			int nonRed = (get_pixel(cameraView, redRow, col, 1)+get_pixel(cameraView, redRow, col, 2))/2;
@@ -64,8 +62,9 @@ RedLines renderImageChallenge(){
 			}
 	}
 
-	int leftSide=0;
-	bool checking = false;
+	int leftSide=0; //size of left red border
+	bool checking = false; //when true, the border has been found
+	/* for columns from  */
 	for(int col=0; col<cameraView.width; col++){
 	for(int row=0; row<cameraView.height; row++){
 		
