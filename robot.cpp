fe0b899447a5825afa956abf38449d82dd1d0eb9
heleneiscool <46540694+heleneiscool@users.noreply.github.com>
2020-06-19 16:12:02 +1200

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,12 +1,41 @@
+
 #include "imageRender.hpp"
 #include "speedDecider.hpp"
+
+
 int main(){
+
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
+	}   
+	bool asking = true;
+	bool core = false;
+	/*
+	while(asking){	
+	std::string level ="";
+	std::cout<<"core/completion/challenge: "<<std::endl;
+	std::cin>>level;
+	if(level.compare("core")==0 |level.compare("completion")==0){
+		asking = false;
 	}
+	else if(level.compare("challenge")==0){
+		asking = false;
+		core =false;
+	}
+	*/
+//}
     while(1){
-		setMotorCore(renderImageCore());  
-        usleep(10000);
-  } //while
+		takePicture();
+		// While true is going in a loop move the car
+		if(core){
+		setMotorCompletion(renderImageCompletion());
+	}
+	else{
+		setMotorChallenge(renderImageChallenge());
+		//setMotorCompletion(renderImageChallenge());
+	}
 
+
+
+  } //while
 } // main
